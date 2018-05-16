f99d0be05e6dbd5211a3c19920a02ac18edce373
Cristóbal Miranda <cristobal.miranda.u@gmail.com>
2018-05-16 15:59:47 -0400

    asociaciones de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,7 @@ class User < ApplicationRecord
 
   has_many :orders
   has_many :products
+
+  has_many :user_order_details, through: :orders, source: 'order_details'
+  has_many :user_payments, through: :orders, source: 'deposits'
 end
